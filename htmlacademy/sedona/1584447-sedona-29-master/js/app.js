cf2605466f1ae676bf737f85ff48e6f22bed7369
Andrey Samoilenko <57430188+asamoile@users.noreply.github.com>
2021-02-24 11:33:21 +0600

    Новые проекты

---
+++ htmlacademy/sedona/1584447-sedona-29-master/js/app.js
@@ -0,0 +1,48 @@
+let buttons = document.querySelectorAll('input[type="button"]');
+let sorting = document.getElementById('text_sorting');
+
+let buttonData = Array.from(buttons).map((el, i) => {
+  el.addEventListener('click', () => {
+    const clicked = !buttonData[i].clicked;
+    buttonData[i].clicked = clicked;
+    if (clicked) {
+      el.classList.add('highlighted');
+    } else {
+      el.classList.remove('highlighted');
+    }
+  });
+  return {element: el, clicked: false}
+});
+
+
+let searchHostel = document.getElementById('search_hostel');
+let search = document.getElementById('search_hostel_calendar');
+
+if (searchHostel) {
+  searchHostel.addEventListener("click", function () {
+    search.classList.toggle("visibility");
+    console.log("1");
+  });
+}
+
+var aaa = $("#text_button_search_hostel");
+
+aaa.click(function (event) {
+  event.preventDefault();
+  $('#form_search_hostel input').each(function (index, element) {
+    if ($(element).val() == "") {
+      $(element).addClass("error");
+
+    } else {
+      $(element).removeClass("error");
+    }
+
+  });
+  if ($('#form_search_hostel input.error').length == 0) {
+    document.location.href = "./seach.html";
+  } else {
+    console.log("ошибка");
+  }
+
+})
+
